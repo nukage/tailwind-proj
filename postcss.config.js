c679b3d960001fa8079e213dc59968f9b4adc149
Tom Myruski <thomas.myruski@gmail.com>
2019-12-12 18:45:24 -0500

    lots of node stuff added, pinegrow added

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
 module.exports = {
     plugins: [
+      require('postcss-import'),
       require('tailwindcss'),
       require('autoprefixer'),
+      require('precss')
+
+      // ALSO LOOK INTO CSSNANO TO MINIFY FOR PRODUCTION, IF I EVER COME BACK HERE....!!! https://flaviocopes.com/tailwind-setup/
+
+      // require('@fullhuman/postcss-purgecss')(
+      //   {
+      //     content: [
+      //       './public/index.html'
+      //     ],
+      //     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      //   })
     ]
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        risered: '#DE225C'
+      }
+    }
   },
   variants: {},
   plugins: []

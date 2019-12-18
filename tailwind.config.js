3fc3449f61a72c8bb9674fb47455e4e91a9a3582
Tom Myruski <thomas.myruski@gmail.com>
2019-12-18 16:36:14 -0500

    Created Video Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
     extend: {
       colors: {
         risered: '#DE225C',
-        risered_1: '#b31948'
+        risered_1: '#b31948',
+        risered_2: '#8c002b',
+        risered_bg: '#170007'
       }
     }
   },
